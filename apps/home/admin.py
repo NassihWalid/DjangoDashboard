0a39012868a06266a69199252d9605c099f0602d
NassihWalid <147839746+NassihWalid@users.noreply.github.com>
2024-05-19 15:14:54 +0100

    tatabase

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -6,3 +6,34 @@ Copyright (c) 2019 - present AppSeed.us
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Driver, Road, Vehicle, Voyage, Alert
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('id_d', 'name', 'phone', 'email')
+    search_fields = ('name', 'phone', 'email')
+
+@admin.register(Road)
+class RoadAdmin(admin.ModelAdmin):
+    list_display = ('id_r', 'name', 'type')
+    search_fields = ('name',)
+    list_filter = ('type',)
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('id_v', 'model', 'year')
+    search_fields = ('model',)
+    list_filter = ('year',)
+
+@admin.register(Voyage)
+class VoyageAdmin(admin.ModelAdmin):
+    list_display = ('id_vo', 'vehicle', 'driver', 'road', 'datev')
+    search_fields = ('vehicle__model', 'driver__name', 'road__name')
+    list_filter = ('datev', 'vehicle__model', 'driver__name', 'road__type')
+
+@admin.register(Alert)
+class AlertAdmin(admin.ModelAdmin):
+    list_display = ('id_a', 'voyage', 'date')
+    search_fields = ('voyage__id_vo',)
+    list_filter = ('date',)
--- apps/home/models.py
+++ apps/home/models.py
@@ -8,3 +8,51 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Driver(models.Model):
+    id_d = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def _str_(self):
+        return self.name
+
+class Road(models.Model):
+    id_r = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    ROAD_TYPES = (
+        ('Auto-route', 'Auto-route'),
+        ('National', 'National'),
+        ('Rurale', 'Rurale'),
+    )
+    type = models.CharField(max_length=20, choices=ROAD_TYPES)
+
+    def _str_(self):
+        return self.name
+
+class Vehicle(models.Model):
+    id_v = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+
+    def _str_(self):
+        return self.model
+
+class Voyage(models.Model):
+    id_vo = models.AutoField(primary_key=True)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='voyages')
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name='voyages')
+    road = models.ForeignKey(Road, on_delete=models.CASCADE, related_name='voyages')
+    datev = models.DateTimeField()
+
+    def _str_(self):
+        return f'Voyage on {self.datev} by {self.driver.name} with {self.vehicle.model} on {self.road.name}'
+
+class Alert(models.Model):
+    id_a = models.AutoField(primary_key=True)
+    voyage = models.ForeignKey(Voyage, on_delete=models.CASCADE, related_name='alerts')
+    date = models.DateTimeField()
+
+    def _str_(self):
+        return f'Alert on {self.date} for Voyage {self.voyage.id_vo}'
+
